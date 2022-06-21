7b2430aa68c767595f7492285612c1ad3e9e9985
Umbrien <serhii.kaluhin@nure.ua>
2022-06-21 13:11:22 +0300

    show amount left

--- lb3/index.php
+++ lb3/index.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+
+$fanta_max = 30;
+$sprite_max = 20;
+$nuts_max = 50;
+
+if (isset($_SESSION['fanta_amount'])) {
+    $fanta_max -= $_SESSION['fanta_amount'];
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,9 +22,18 @@
     <?php include 'header.phtml' ?>
     <div class="container">
     <form action="cart.php" method="POST">
-        <input type="checkbox" name="fanta_bool"> Fanta <input type="number" value="0" name="fanta_amount"> $6 <br>
-        <input type="checkbox" name="sprite_bool"> Sprite <input type="number" value="3" name="sprite_amount"> $14 <br>
-        <input type="checkbox" name="nuts_bool"> Nuts <input type="number" value="0" name="nuts_amount"> $5.2 <br>
+        <?php
+            echo '<input type="checkbox" name="fanta_bool"> Fanta <input type="number" value="0" name="fanta_amount" max="' . $fanta_max . '"> $6 ';
+            echo ' | ' . $fanta_max . ' left <br>';
+        ?>
+        <?php
+            echo '<input type="checkbox" name="sprite_bool"> Sprite <input type="number" value="3" name="sprite_amount" max="' . $sprite_max . '"> $14';
+            echo ' | ' . $sprite_max . ' left <br>';
+        ?>
+        <?php
+            echo '<input type="checkbox" name="nuts_bool"> Nuts <input type="number" value="0" name="nuts_amount" max="' . $nuts_max . '"> $5.2';
+            echo ' | ' . $nuts_max . ' left <br>';
+        ?>
 
         <input type="submit" value="Send" class="btn btn-success">
     </form>
