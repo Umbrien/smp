d79f9081fdc0b0432b052a6ada930aab666fb196
Umbrien <serhii.kaluhin@nure.ua>
2022-07-04 01:29:59 +0300

    add 404 page

---
+++ lb4/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Not found</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/index.css" />
+</head>
+<body>
+    <?php include 'header.phtml' ?>
+    <h1>404</h1>
+    <h3>Not found</h3>
+    <?php include 'footer.html' ?>
+</body>
+</html>
